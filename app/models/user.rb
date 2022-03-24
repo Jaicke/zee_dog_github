fa397f3ade957f63d7534d4aa2153a6794bcf49d
Jaicke <jaicke.silva@mesainc.com.br>
2022-03-23 23:48:00 -0300

    feat(jwt-auth): Adds User model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, :email, :password, :password_confirmation, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { minimum: 8 }, confirmation: true
+end
