90a26fd2fbd31fb748f2035f8c32c4d6ce726dcf
Jaicke <jaicke.silva@mesainc.com.br>
2022-03-26 03:11:52 -0300

    test: Adds request helper for tests with authenticated  user

--- spec/support/request_spec_helper.rb
+++ spec/support/request_spec_helper.rb
@@ -2,4 +2,10 @@ module RequestSpecHelper
   def json_body
     JSON.parse(response.body, symbolize_names: true) if response.body.present?
   end
+
+  def user_auth_token_header(user)
+    post api_v1_authentications_path, params: { email: user.email, password: user.password }
+
+    { Authorization: "Bearer #{json_body[:token]}" }
+  end
 end
